df0bcbe5366de55dc6d9126e391a77563510a763
john de winton <john.dewinton@yahoo.co.uk>
2019-07-05 14:10:33 +0100

    updated routes so I can reach the projects and contact view pages from the root page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get '/about', { to: 'pages#about', as: 'about' }
-  get '/about', { to: 'pages#projects', as: 'projects' }
-  get '/about', { to: 'pages#contact', as: 'contact' }
+  get '/projects', { to: 'pages#projects', as: 'projects' }
+  get '/contact', { to: 'pages#contact', as: 'contact' }
 
 
 end
