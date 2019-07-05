97df163c7937f9578e0b5f8d27746dc60cc32e7c
john de winton <john.dewinton@yahoo.co.uk>
2019-07-05 13:04:37 +0100

    added routes for about, projects and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
+  get '/about', { to: 'pages#about', as: 'about' }
+  get '/about', { to: 'pages#projects', as: 'projects' }
+  get '/about', { to: 'pages#contact', as: 'contact' }
+
+
 end
